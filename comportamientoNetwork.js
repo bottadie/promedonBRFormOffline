d464e54ebf49abc374f53c6898ba2c55530179dc
DiegoBotta <63814283+DiegoBotta@users.noreply.github.com>
2021-10-22 23:01:07 -0300

    conexión finalizada IDB & Firestore

--- comportamientoNetwork.js
+++ comportamientoNetwork.js
@@ -1,16 +1,19 @@
+import { collection, query, where, getDocs,doc,setDoc,addDoc } from 'https://www.gstatic.com/firebasejs/9.1.0/firebase-firestore.js';
+
+import { db } from '/firebase.js';
 
 function cargaIndexToFirestore(){
     var request = window.indexedDB.open('notasBd');
         request.onsuccess = function(event) {
             // Use this db variable, not your global one
-            var db = event.target.result;
+            var dbI = event.target.result;
     
                 // Note that you can also access the db variable using other means
             // here like this.result or request.result, but I like to use event.target
             // for clarity.
     
             // Now work with the db variable
-            let transaction = db.transaction('notas','readwrite')
+            let transaction = dbI.transaction('notas','readwrite')
             let notas = transaction.objectStore("notas"); // (2)
          
           var request = notas.getAll()
@@ -21,10 +24,18 @@ function cargaIndexToFirestore(){
             console.log('No se tiene todavia')
           };
           
-          request.onsuccess = function(event) {
+          request.onsuccess = async function(event) {
            var resultado = request.result;
-           for (var i=0; i<resultado.length; i++) { console.log(resultado[i].contenido) }
-
+           console.log(db)
+           for (var i=0; i<resultado.length; i++) {
+           
+              await addDoc(collection(db, "Notas"), {
+                Nota: resultado[i].contenido,
+              })
+               }
+               let transaccion = dbI.transaction(['notas'], 'readwrite').objectStore('notas');
+               var keyRange = IDBKeyRange.upperBound("0")
+               transaccion.delete(keyRange)
         
            
            
